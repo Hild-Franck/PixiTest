de5eadfa6b07d58cd3b78de50a099c17ae6e2886
Hild-Franck <franck.hild@gmail.com>
2016-02-20 11:44:47 +0100

    Controls system - Set the controls list - Display control's list on view

--- app/app.js
+++ app/app.js
@@ -1,12 +1,77 @@
-'use strict';
+(function () {
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    var app = angular.module('myApp', []);
+
+    app.controller('ControlsController', function () {
+
+
+        this.stage = stage;
+        this.renderer = renderer;
+
+        document.body.appendChild(this.renderer.view);
+
+        requestAnimationFrame(animate);
+        console.log(this.stage);
+        // create a texture from an image path
+        var texture = PIXI.Texture.fromImage("ressources/bunny.png");
+        // create a new Sprite using the texture
+        var bunny = new PIXI.Sprite(texture);
+
+        // center the sprites anchor point
+        bunny.anchor.x = 0.5;
+        bunny.anchor.y = 0.5;
+
+        // move the sprite t the center of the screen
+        bunny.position.x = 200;
+        bunny.position.y = 150;
+
+        bunny.scale.x = 2;
+        bunny.scale.y = 2;
+
+
+        stage.addChild(bunny);
+
+        function animate() {
+
+            requestAnimationFrame(animate);
+
+            // Place your animation code between here...
+            bunny.rotation += 0.1;
+            // ...and here
+
+            renderer.render(stage);
+        }
+        // Here is your controls
+        this.functions = [
+            {
+                /**
+                 * @var string name
+                 * Name of your variable(s) control
+                 */
+                name: "myExample",
+                /**
+                 * @param value
+                 * Function that change something in your render with value
+                 */
+                onChange: function(value){
+                    console.log(value);
+                }
+
+            }
+        ]
+
+
+    });
+
+    // - Pixi Initialization -
+    // Pixi stage creation
+    var stage = new PIXI.Stage(0x66FF99);
+
+    // Renderer creation
+    var renderer = PIXI.autoDetectRenderer(400, 300);
+
+
+
+
+})();
