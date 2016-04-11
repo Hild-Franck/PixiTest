9d8c9097e9c513ceda25bcbac318b9275117b5c9
Knarfux <knarfux@gmail.com>
2016-04-11 11:54:17 +0200

    New dev tasks

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        notify_hooks: {
+            options: {
+                enabled: true,
+                max_jshint_notifications: 1, // maximum number of notifications from jshint output
+                title: "TartiGame", // defaults to the name in package.json, or will use project directory's name
+                success: false, // whether successful grunt executions should be notified automatically
+                duration: 3 // the duration of notification in seconds, for `notify-send only
+            }
+        },
+
+        bower: {
+            install: {
+                options: {
+                    install: true,
+                    copy: false,
+                    targetDir: './libs',
+                    cleanTargetDir: true
+                }
+            }
+        },
+        jshint:{
+            options:{
+                force: true
+            },
+            target: ['Gruntfile.js',"unit-tests/*.js" , "app/*.js"]
+        },
+        karma:{
+            options:{
+                configFile: 'karma.conf.js'
+            },
+            continuous: {
+                singleRun: true,
+                autoWatch: true
+            },
+            single:{
+                singleRun: true,
+                autoWatch: false
+            }
+        },
+        watch:{
+            options:{
+                livereload: true
+            },
+            js:{
+                files: ['app/*.js', 'tests/*js', 'app/*.html'],
+                tasks:['concurrent:target'],
+                options: {
+                    atBegin: true
+                }
+            }
+        },
+        concurrent:{
+            target:{
+                options:{
+                    logConcurrentOutput: true
+                },
+                tasks: [['jshint'], 'karma:continuous']
+            }
+
+        },
+        concat:{
+            js:{
+                src: ['app/main.js', 'app/Array/*.js', 'app/Class/*.js'],
+                dest: 'app/dist/build.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.loadNpmTasks('grunt-notify');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.task.run('notify_hooks');
+
+    grunt.registerTask('dev', ['jshint', 'watch']);
+    grunt.registerTask('pixiTest', ['jshint', "karma:single", "concat"]);
+    grunt.registerTask('code', ['jshint']);
+};
--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,50 @@
-module.exports = function(config){
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    basePath : './',
+        basePath: './',
 
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
-    ],
+        files: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/components/**/*.js',
+            'app/view*/**/*.js'
+        ],
 
-    autoWatch : true,
+        autoWatch: true,
 
-    frameworks: ['jasmine'],
+        frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+        browsers: ['Chrome'],
 
-    plugins : [
+        plugins: [
+            "karma-spec-reporter",
+            "karma-growl-reporter",
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
             'karma-junit-reporter'
-            ],
+        ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+        reporters: ["spec", "growl"],
+        specReporter: {
+            maxLogLines: 5,         // limit number of lines logged per test
+            suppressErrorSummary: true,  // do not print error summary
+            suppressFailed: false,  // do not print information about failed tests
+            suppressPassed: false,  // do not print information about passed tests
+            suppressSkipped: true  // do not print information about skipped tests
+        },
 
-  });
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
+
+    });
 };
